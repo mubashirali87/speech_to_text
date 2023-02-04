7398fa50acbcfd2fa93b1bb5648e194bf2e335b9
mubashirali87 <44904844+mubashirali87@users.noreply.github.com>
2023-02-04 20:29:29 +0530

    Create speech_to_text.py

---
+++ speech_to_text.py
@@ -0,0 +1,12 @@
+import speech_recognition as sr
+
+# initialize recognizer class (for recognizing the speech)
+r = sr.Recognizer()
+
+# Reading Microphone as source
+# listening the speech and store in audio_text variable
+with sr.Microphone() as source:
+    print("Talk")
+    audio_text = r.listen(source)
+    print("Time over, thanks")
+    print("Text: "+r.recognize_google(audio_text))
